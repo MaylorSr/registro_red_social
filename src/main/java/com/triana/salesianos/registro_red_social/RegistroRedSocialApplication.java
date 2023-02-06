ba93430fd38bfdcff9c3992c9ac41b3c887bf530
Maylor David Bustamante Mercado <maylorbustamante2001@gmail.com>
2023-02-06 15:47:32 +0100

    Se finalizó el ejemplo, está todo correcto

--- src/main/java/com/triana/salesianos/registro_red_social/RegistroRedSocialApplication.java
+++ src/main/java/com/triana/salesianos/registro_red_social/RegistroRedSocialApplication.java
@@ -3,7 +3,6 @@ package com.triana.salesianos.registro_red_social;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
 @SpringBootApplication
 @EnableJpaAuditing
 public class RegistroRedSocialApplication {
@@ -11,5 +10,4 @@ public class RegistroRedSocialApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RegistroRedSocialApplication.class, args);
 	}
-
 }
--- src/main/java/com/triana/salesianos/registro_red_social/model/User.java
+++ src/main/java/com/triana/salesianos/registro_red_social/model/User.java
@@ -1,25 +1,32 @@
 package com.triana.salesianos.registro_red_social.model;
 
-import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
 
 @Entity
-@Data
-@AllArgsConstructor
-@Builder
 @NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Table(name = "_user")
 public class User {
     @Id
     @GeneratedValue
     private Long id;
+    @Column(name = "user_name")
 
     private String userName;
 
+    @Column(name = "email")
     private String email;
 
+    @Column(name = "password")
     private String password;
 
 
