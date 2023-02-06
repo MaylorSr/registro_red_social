374d7984c0853e48deda05ffafcb251cc85c04f2
Maylor <maylorbustamante2001@gmail.com>
2023-02-06 11:48:14 +0100

    Se ha creado el proyecto con sus carpetas bases

---
+++ src/main/java/com/triana/salesianos/registro_red_social/RegistroRedSocialApplication.java
@@ -0,0 +1,13 @@
+package com.triana.salesianos.registro_red_social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroRedSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroRedSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/triana/salesianos/registro_red_social/RegistroRedSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triana.salesianos.registro_red_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroRedSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
